69fbbb38b3cf51618d69c5b7551ba6f3df8eace0
Caetano Alysson <122699973+AlyssonCaetano@users.noreply.github.com>
2025-08-18 16:26:19 -0300

    Addting Menu background and music

---
+++ code/Const.py
@@ -0,0 +1,3 @@
+#Definir tamanho padrão da tela do jogo
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/game.py
+++ code/game.py
@@ -3,15 +3,22 @@
 
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        #Definir tamanho da janela do windows
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        #Adicionar/carregar musica
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        #Tocar a musica
+        pygame.mixer_music.play(-1)
+
         while True:
             #chamar tela de menu
             menu = Menu(self.window)
