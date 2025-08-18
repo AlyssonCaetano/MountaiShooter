cd2caf5b125b391035d05cfee74a08fdcc03f140
Caetano Alysson <122699973+AlyssonCaetano@users.noreply.github.com>
2025-08-18 17:22:39 -0300

    Addting Menu Texte, color and position.

--- code/game.py
+++ code/game.py
@@ -14,20 +14,10 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-        #Adicionar/carregar musica
-        pygame.mixer_music.load('./asset/Menu.mp3')
-        #Tocar a musica
-        pygame.mixer_music.play(-1)
-
-        while True:
-            #chamar tela de menu
-            menu = Menu(self.window)
-            menu.run()
-            pass
+            while True:
+                #chamar tela de menu
+                menu = Menu(self.window)
+                menu.run()
+                pass
 
 
-            # Check for all events
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit() # Close window
-            #         quit() # end pygame
